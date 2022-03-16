0bd5f24362c7c61339a8975556546bc2bc83fa99
abnuo <66645994+abnuo@users.noreply.github.com>
2022-03-16 14:15:51 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name="twitter-selenium",
+   version="1.0",
+   description="Tweeting with selenium",
+   packages=["twitter_selenium"],  #same as name
+   install_requires=["selenium"], #external packages as dependencies
+   scripts=[
+            "scripts/tweet.py",
+           ]
+)
