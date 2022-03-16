eb138fb3b9b7561d1207bf1b1d55c324b485213b
abnuo <foo@bar.com>
2022-03-16 15:55:32 +0000

    kwfjoj

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name="twitter-selenium",
-   version="1.4",
+   version="1.5",
    description="Tweeting with selenium",
    packages=["twitter_selenium"],
    install_requires=["selenium"],
