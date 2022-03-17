150e3d410a16147c556385425a47b6fe36becfc0
abnuo <66645994+abnuo@users.noreply.github.com>
2022-03-17 01:26:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name="twitter-selenium",
-   version="3.4",
+   version="3.5",
    description="Tweeting with selenium",
    packages=["twitter_selenium"],
    install_requires=["selenium","BeautifulSoup4"],
